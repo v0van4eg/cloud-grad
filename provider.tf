68c46156a4b078d5a949237e8d5335b9491e5d8c
poly <poly@vldm.ru>
2025-12-24 22:43:15 +0300

    dsd

--- provider.tf
+++
@@ -1,6 +0,0 @@
-provider "yandex" {
-  service_account_key_file = "./key.json"  # Файл ключа сервисного аккаунта
-  cloud_id                 = var.cloud_id
-  folder_id                = var.folder_id
-  zone                     = var.zone
-}
