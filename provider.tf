b7279a3a19ee271d2a7820b103e4f424e2df17da
poly <poly@vldm.ru>
2025-12-24 22:43:55 +0300

    22

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "yandex" {
+  service_account_key_file = "./key.json" # Файл ключа сервисного аккаунта
+  cloud_id                 = "b1gmjlsenka24ksvre01"
+  folder_id                = "b1g00u19k2fc6qj4q9vk"
+  zone                     = "ru-central1-a"
+}
